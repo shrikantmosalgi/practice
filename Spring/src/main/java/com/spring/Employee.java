55e80c35a4b127c7d3f4c50fda27c82855d97b8f
shrikantmosalgi <shrikantmosalgi1997@gmail.com>
2025-02-06 18:01:05 +0530

    collections

--- Spring/src/main/java/com/spring/Employee.java
+++ Spring/src/main/java/com/spring/Employee.java
@@ -1,28 +1,32 @@
 package com.spring;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 	private int empId;
 	private String empName;
 	private int empSal;
-	private String empAddress;
-	
+	private List<String> empAddresses;
+	private Set<String> phones;
+  	private Map<String, String> courses;
 	
 	
 	public Employee() {
 				
 	}
-	
-	
-	
-	public Employee(int empId, String empName, int empSal, String empAddress) {
-		super();
+
+	public Employee(int empId, String empName, int empSal, List<String> empAddresses, Set<String> phones,
+			Map<String, String> courses) {
+		
 		this.empId = empId;
 		this.empName = empName;
 		this.empSal = empSal;
-		this.empAddress = empAddress;
+		this.empAddresses = empAddresses;
+		this.phones = phones;
+		this.courses = courses;
 	}
-
-
 
 	public int getEmpId() {
 		return empId;
@@ -42,16 +46,35 @@ public class Employee {
 	public void setEmpSal(int empSal) {
 		this.empSal = empSal;
 	}
-	public String getEmpAddress() {
-		return empAddress;
+
+	public List<String> getEmpAddresses() {
+		return empAddresses;
 	}
-	public void setEmpAddress(String empAddress) {
-		this.empAddress = empAddress;
+
+	public void setEmpAddresses(List<String> empAddresses) {
+		this.empAddresses = empAddresses;
 	}
+
+	public Set<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(Set<String> phones) {
+		this.phones = phones;
+	}
+
+	public Map<String, String> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(Map<String, String> courses) {
+		this.courses = courses;
+	}
+
 	@Override
 	public String toString() {
-		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empAddress=" + empAddress
-				+ "]";
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empAddresses="
+				+ empAddresses + ", phones=" + phones + ", courses=" + courses + "]";
 	}
 	
 	
