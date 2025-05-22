01d84680fd7562fd78568752895fe0d035da8461
shrikantmosalgi <shrikantmosalgi1997@gmail.com>
2025-05-22 09:41:25 +0530

    new spring batch rhs project

---
+++ SpringBatchRhs/src/main/java/com/springBatch/SpringBatchRhsApplication.java
@@ -0,0 +1,13 @@
+package com.springBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchRhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchRhsApplication.class, args);
+	}
+
+}
